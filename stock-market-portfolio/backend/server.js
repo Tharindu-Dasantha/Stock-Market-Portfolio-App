c4421723aa68f36b89be90b1614306678d16dcfb
Dasantha Edirisinghe <114149683+Tharindu-Dasantha@users.noreply.github.com>
2024-08-05 12:39:14 +0530

    added the uri to the .env file

--- stock-market-portfolio/backend/server.js
+++ stock-market-portfolio/backend/server.js
@@ -3,16 +3,21 @@ const mongoose = require("mongoose");
 const cors = require("cors");
 const bodyParser = require("body-parser");
 
+require("dotenv").config();
+
 const app = express();
 const PORT = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(bodyParser.json());
 
-mongoose.connect("mongodb+srv://cs50tharindu:tj9aQM5ET1uwWcnU@stocklist.o2hwnc0.mongodb.net/Stocks", {
+mongoose.connect(process.env.MONGO_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
-});
+})
+  .then(() => console.log("Connected to MongoDB"))
+  .catch(err => console.error("MongoDB connection error", err));
+
 
 const stockShema = new mongoose.Schema({
   company: String,
